267976cb643289d1d6ee2d5c3df6b24bc33444a6
Joachim Roeleveld <joachimroeleveld@gmail.com>
2019-01-31 11:57:27 +0100

    Add gradient module

--- android/app/src/main/java/com/nightguideapp/MainApplication.java
+++ android/app/src/main/java/com/nightguideapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.nightguideapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.BV.LinearGradient.LinearGradientPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.reactcommunity.rnlanguages.RNLanguagesPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new LinearGradientPackage(),
             new SplashScreenReactPackage(),
             new RNGestureHandlerPackage(),
             new RNLanguagesPackage(),
