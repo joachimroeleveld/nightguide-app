fd1609cc5434e59310f76532b9abb00d932a882e
Joachim Roeleveld <joachimroeleveld@gmail.com>
2019-02-14 17:50:21 +0100

    RN upgrade

---
+++ babel.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset', '@babel/preset-flow'],
+  plugins: [
+    '@babel/plugin-transform-flow-strip-types',
+    '@babel/plugin-proposal-class-properties',
+  ],
+};
